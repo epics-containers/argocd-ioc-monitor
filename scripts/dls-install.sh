aeb4acaa2aa7b9515d207960ab456ad19af4a047
Giles Knap <giles.knap@diamond.ac.uk>
2026-04-20 14:22:25 +0000

    switch to live argocd

--- scripts/dls-install.sh
+++ scripts/dls-install.sh
@@ -7,11 +7,11 @@ helm upgrade --install argocd-monitor oci://ghcr.io/epics-containers/charts/argo
     --set image.tag=0.5.2 \
     --set oauth2Proxy.enabled=true \
     --set oauth2Proxy.clientId=argocd-monitor \
-    --set oauth2Proxy.issuerUrl=https://argocd-test.diamond.ac.uk/api/dex \
+    --set oauth2Proxy.issuerUrl=https://argocd.diamond.ac.uk/api/dex \
     --set oauth2Proxy.existingSecret=argocd-monitor-oauth2 \
     --set ingress.enabled=true \
     --set ingress.host=argocd-monitor.diamond.ac.uk \
-    --set argocd.url=https://argocd-test.diamond.ac.uk \
+    --set argocd.url=https://argocd.diamond.ac.uk \
     --set 'oauth2Proxy.extraArgs[0]=--scope=openid profile email audience:server:client_id:argocd' \
     --set 'oauth2Proxy.extraArgs[1]=--redirect-url=https://argocd-monitor.diamond.ac.uk/oauth2/callback' \
     --set 'oauth2Proxy.extraArgs[2]=--cookie-name=_oauth2_proxy_monitor' \
